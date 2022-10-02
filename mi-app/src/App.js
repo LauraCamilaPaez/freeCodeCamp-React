d971d39bea9f7f22717585b56a16fcbc24cc5236
Laura Camila Páez Barbosa <2104laura2104@gmail.com>
2022-10-01 23:50:57 -0500

    add props

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -2,11 +2,22 @@ import './App.css';
 import Testimony from './components/Testimony';
 
 function App() {
-  return (
-    <div className="App">
-      <Testimony />
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className='container'>
+                <h1>Here is what our alumni say about freeCodeCamp:</h1>
+                <Testimony
+                    name='Emma Bostian'
+                    country='Sweden'
+                    position='Software Engineer'
+                    company='Spotify'
+                    testimony='I ve always struggled with learning JavaScript. I ve taken many courses but freeCodeCamps
+                course was the one which stuck. Studying JavaScript as well as data structures and algorithms on
+                freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at
+                Spotify.'/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- mi-app/src/components/Testimony.js
+++ mi-app/src/components/Testimony.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import '../styles-sheets/Testimony.css';
 
-function Testimony() {
-	return (
-		<div className='container-testimony'>
-			<img alt='mark' src={require('../images/testimony-emma.png')} className='image-testimony'/>
-			<div className='container-testimony-text'>
-				<p className='name-testimony'>Alessandra Ferri</p>
-				<p className='position-testimony'>Systems engineer at Spotify</p>
-				<p className='text-testimony'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
-			</div>
-		</div>
-	)
-};
+function Testimony(props) {
+    return (
+            <div className='container-testimony'>
+                <img alt='mark' src={require('../images/testimony-emma.png')} className='image-testimony'/>
+                <div className='container-testimony-text'>
+                    <p className='name-testimony'>{props.name} in {props.country}</p>
+                    <p className='position-testimony'>{props.position} at {props.company}</p>
+                    <p className='text-testimony'>"{props.testimony}"</p>
+                </div>
+            </div>
+    );
+}
 
 export default Testimony;

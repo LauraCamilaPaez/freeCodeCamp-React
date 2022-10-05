b2f272ae663e08303a5e0dacb5afd0660fafe4fe
Laura Camila Páez Barbosa <2104laura2104@gmail.com>
2022-10-04 20:49:53 -0500

    Add content props

--- mi-app/src/components/Testimony.js
+++ mi-app/src/components/Testimony.js
@@ -4,14 +4,17 @@ import '../styles-sheets/Testimony.css';
 function Testimony(props) {
     return (
             <div className='container-testimony'>
-                <img alt='mark' src={require('../images/testimony-emma.png')} className='image-testimony'/>
+                <img alt='mark' src={require(`../images/testimony-${props.image}.png`)} className='image-testimony'/>
                 <div className='container-testimony-text'>
-                    <p className='name-testimony'>{props.name} in {props.country}</p>
-                    <p className='position-testimony'>{props.position} at {props.company}</p>
+                    <p className='name-testimony'><strong>{props.name}</strong> in {props.country}</p>
+                    <p className='position-testimony'>{props.position} at <strong>{props.company}</strong></p>
                     <p className='text-testimony'>"{props.testimony}"</p>
                 </div>
             </div>
     );
 }
+
+// Retos
+// 
 
 export default Testimony;
